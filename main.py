fca981c0f5eaaf12c1d5cdf1f2ddb7c54a78a9d6
Avneet Sekhon <avneetsekhonfp@gmail.com>
2023-03-27 20:54:30 -0700

    Grabbing a specific elements content

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ Deactivate venv: Deactivate
 
 import requests
 import bs4
+
+result = requests.get("https://example.com")
+#print(result) returns <Response [200]>
+
+soup = bs4.BeautifulSoup(result.text, "lxml")
+# print(soup) returns the html of website
+soupTitle = soup.select("title")
+# print(soupTitle) return the specified tags with their content
+print(soupTitle[0].getText()) #Return the first title's content
