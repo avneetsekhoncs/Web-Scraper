5ffa5e450556957eee91803af9f171ab58c624d8
Avneet Sekhon <avneetsekhonfp@gmail.com>
2023-03-28 21:31:37 -0700

    Update: Scarping Images

--- main.py
+++ main.py
@@ -8,17 +8,33 @@ Deactivate venv: Deactivate
 import requests
 import bs4
 
+
+"""""""""""""""""""""
+Scraping a Title
+"""""""""""""""""""""
 result = requests.get("https://example.com")
-#print(result) #returns <Response [200]>
+soup = bs4.BeautifulSoup(result.text, "lxml")
+print(soup.select("title")[0].getText()) #Return the first title's content
 
-#soup = bs4.BeautifulSoup(result.text, "lxml")
-#print(soup) #returns the html of website
-#soupTitle = soup.select("title")
-#print(soupTitle) #return the specified tags with their content
-#print(soupTitle[0].getText()) #Return the first title's content
-
+"""""""""""""""""""""
+Scraping a Class
+"""""""""""""""""""""
 grace_hopper = requests.get("https://en.wikipedia.org/wiki/Grace_hopper")
 soup = bs4.BeautifulSoup(grace_hopper.text, "lxml")
 
 for item in soup.select(".vector-toc-text"):
     print(item.getText())
+
+"""""""""""""""""""""
+Scraping an Image
+"""""""""""""""""""""
+image = requests.get("https://en.wikipedia.org/wiki/Deep_Blue_(chess_computer)")
+soup = bs4.BeautifulSoup(image.text, "lxml")
+
+kasparov = soup.select(".thumbimage")[0]
+#print(kasparov["src"])
+image_link = requests.get("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Kasparov_Magath_1985_Hamburg-2.png/220px-Kasparov_Magath_1985_Hamburg-2.png")
+#print(image_link.content)  #This gets the binary file of the image
+test_file = open("kasparov_img.png", "wb") #make sure file extension is the same as the src. Gets saved in working directory
+test_file.write(image_link.content)
+test_file.close()
